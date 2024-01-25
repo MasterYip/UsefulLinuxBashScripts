d48089a63e577c3de1f46c69cc7c4d2e5966fc55
RaymonYip-NUC11 <2205929492@qq.com>
2024-01-25 11:28:22 +0800

    feat: add red_shift & short cut utils

--- shortcuts/red_shift.sh
+++ shortcuts/red_shift.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 script_dir="$(dirname "$0")"
 source $script_dir/../util_lib.sh
+
+install_package redshift
+set_shortcut RedShiftEnable "redshift -o 12000" "<Primary><Super>x"
+set_shortcut RedShiftEnable "redshift -x" "<Primary><Super>z"
--- util_lib.sh
+++ util_lib.sh
@@ -1,27 +1,76 @@
 # !/bin/bash
+
+PASSWORD="tzzm"
+
 # Fetch script dir using dirname cmd
-script_dir="$(dirname "$0")"
-source $script_dir/config.sh
+# script_dir="$(dirname "$0")"
+# source $script_dir/config.sh
 
 function install_package(){
     # $1: package name
     # $2: package installation command
     if ! command -v $1 > /dev/null; then
         echo "$1 is not installed. Installing..."
-        echo $PASSWORD | sudo apt install $1
+        echo $PASSWORD | sudo -S apt install $1
     else
         echo "$1 is already installed."
     fi
 }
 
-function set_shortcut(){
-    # $1: shortcut name
-    # $2: shortcut command
-    # $3: shortcut binding
-    echo "Setting system shortcut $1"
-    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "$1"
-    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "$2"
-    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "$3"
+# function set_shortcut(){
+#     # $1: shortcut name
+#     # $2: shortcut command
+#     # $3: shortcut binding
+#     echo "Setting system shortcut $1"
+#     # 列出已存在的自定义快捷键
+#     existing_custom_keys=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
+#     # num_existing_custom_keys=$(grep -o "," <<< "$existing_custom_keys" | wc -l)
+#     num_existing_custom_keys=$(grep -o "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" <<< "$existing_custom_keys" | wc -l)
+#     echo "Number of existing custom keybindings: $num_existing_custom_keys"
 
-    echo "Done! Shortcut is set to $3."
+#     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name "$1"
+#     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command "$2"
+#     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding "$3"
+
+#     echo "Done! Shortcut is set to $3."
+# }
+
+function set_shortcut(){
+    # 定义要使用的键和字符串
+    key="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding"
+    subkey1="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+    subkey1=${subkey1%?}":"
+    firstname="custom"
+
+    # 获取当前自定义快捷键列表
+    array_str=$(gsettings get "$key")
+
+    # 如果数组为空，移除注释提示
+    command_result=${array_str//@as/}
+    current=($(eval "$command_result"))
+
+    # 确保新的快捷键不是重复的
+    n=1
+    while true; do
+        new="$item_s$firstname$n/"
+        if [[ " ${current[@]} " =~ " $new " ]]; then
+            ((n++))
+        else
+            break
+        fi
+    done
+
+    # 将新的快捷键添加到列表中
+    current+=("$new")
+
+    # 创建快捷键，设置名称、命令和快捷键
+    cmd0="gsettings set $key '$(printf "%s\n" "${current[@]}")'"
+    cmd1="gsettings set $key:$subkey1$new name '$1'"
+    cmd2="gsettings set $key:$subkey1$new command '$2'"
+    cmd3="gsettings set $key:$subkey1$new binding '$3'"
+
+    # 执行命令
+    for cmd in "$cmd0" "$cmd1" "$cmd2" "$cmd3"; do
+        eval "$cmd"
+    done
 }
