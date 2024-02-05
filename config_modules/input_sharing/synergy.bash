2a62a4394e3bd298d187fbb55eff2ee798e975cd
HexLab-NUC12-MasterYip <2205929492@qq.com>
2024-02-05 21:34:44 +0800

    chore: update synergy

--- config_modules/input_sharing/synergy.bash
+++ config_modules/input_sharing/synergy.bash
@@ -2,7 +2,7 @@
 wget -O /tmp/synergy.deb  http://archive.ubuntu.com/ubuntu/pool/universe/s/synergy/synergy_1.8.8-stable+dfsg.1-1build1_amd64.deb
 # echo $PASSWORD | sudo -S apt install /tmp/synergy.deb
 echo $PASSWORD | sudo -S add-apt-repository ppa:rock-core/qt4
-sudo apt-get update
-sudo apt-get install libcanberra-gtk-module
-sudo dpkg -i /tmp/synergy.deb
-sudo apt-get -f install
+echo $PASSWORD | sudo -S apt-get update
+echo $PASSWORD | sudo -S apt-get install -y libcanberra-gtk-module
+echo $PASSWORD | sudo -S dpkg -i /tmp/synergy.deb
+echo $PASSWORD | sudo -S apt-get -f install
